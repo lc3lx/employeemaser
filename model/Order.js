41b6efc668f7e052497722bec3be4c1610cdaae9
lc3lx <140909706+lc3lx@users.noreply.github.com>
2024-07-30 17:38:48 +0300

    Order.js

--- model/Order.js
+++ model/Order.js
@@ -8,26 +8,31 @@ const orderschema = new mongoose.Schema(
         type:String ,
         minlength : 3,
         maxlength : 200,
+        required : true
      
     },
     customarphone :{
         type : String ,
         minlength:9,
         maxlength:14,
+        required : true
       
     },
     locationCar : {
         type: String,
+        required : true
        
         
     },
     timeofserves :{
         type:String ,
+        required : true
         
 
     },
     local:{
-        type:String,
+        type:Date,
+        required : true
        
     }
 
