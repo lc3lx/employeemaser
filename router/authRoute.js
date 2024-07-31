0af12b69a80f83d57656d5287929b97526c86644
lc3lx <140909706+lc3lx@users.noreply.github.com>
2024-07-31 14:52:04 +0300

    Delete router/authRoute.js

--- router/authRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const { registerUserCtrl, loginUserCtrl } = require("../controller/authcontroller")
-const { verifyToken } = require("../middlewere/aurthmiddlewere")
-
-router.post("/register",registerUserCtrl)
-router.post("/login",loginUserCtrl)
-module.exports = router
