c1c11110280c05ee54212c339298a57cd8c78441
lc3lx <140909706+lc3lx@users.noreply.github.com>
2024-07-31 14:53:20 +0300

    recyclbin.js

--- router/recyclbin.js
+++ router/recyclbin.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
 const {getthedata,resrverdata} = require("../controller/recyclbincontroller")
-const { verifyToken } = require("../middlewere/aurthmiddlewere")
+
 router.post("/sendorder",resrverdata)
 router.get("/getorder",getthedata)
 router.delete("/deleteorder/:id")
