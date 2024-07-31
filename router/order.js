6b90838a210e3d90c855588e5ebdf41e8ab13bcc
lc3lx <140909706+lc3lx@users.noreply.github.com>
2024-07-31 14:47:00 +0300

    order.js

--- router/order.js
+++ router/order.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
 const {getthedata,resrverdata,updatedata,deletedata} = require("../controller/ordercontroller")
-const { verifyToken } = require("../middlewere/aurthmiddlewere")
+
 
 
 router.post("/postorder",resrverdata)
